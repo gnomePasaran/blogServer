505ac5cae45b2ccefc147d7ad16a35643cb815ca
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-02-04 23:44:55 +0300

    add some

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ application.get('/', function(req, res) {
 });
 
 application.get('/posts/:id', function(req, res) {
+  postId = parseInt(req.params.id);
+  res.json(
+    myBlogs.slice((postId - 1) * 3, 3 * (postId))
+  );
+});
+
+application.get('/post/:id', function(req, res) {
   res.json(myBlogs[req.params.id - 1]);
 });
 
