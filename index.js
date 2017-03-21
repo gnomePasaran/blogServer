d7c13fb95c2ba266ee19f42d111843ea008f70de
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-03-22 01:18:47 +0300

    changes for middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var application = express();
+var _ = require('lodash');
 
 var cors = require('cors');
 
@@ -8,18 +9,33 @@ var myBlogs = require('./data').myBlogs
 application.use(cors());
 
 application.get('/', function(req, res) {
-  res.json(myBlogs);
+  pageId = req.query.page ? parseInt(req.query.page) : 1
+  result = {
+    items: myBlogs.slice((pageId - 1) * 3, 3 * (pageId)),
+    count: myBlogs.length
+  };
+  res.json(result);
 });
 
-application.get('/posts/:id', function(req, res) {
-  postId = parseInt(req.params.id);
-  res.json(
-    myBlogs.slice((postId - 1) * 3, 3 * (postId))
-  );
+application.post('/post/:id/like', function(req, res) {
+  item = myBlogs[req.params.id - 1];
+  item.meta.count++;
+  res.json(item);
 });
 
 application.get('/post/:id', function(req, res) {
   res.json(myBlogs[req.params.id - 1]);
+});
+
+application.get('/search', function(req, res) {
+  var rgxp = new RegExp(req.query.q, 'i');
+  var foundBlogs = _.filter(myBlogs,
+    function (blog) {
+      if (blog.text.search(rgxp) > 0)
+        return blog;
+    });
+
+  res.json(foundBlogs);  
 });
 
 application.listen(3001, function() {
