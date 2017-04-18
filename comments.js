a23b83fef6cfa42a937fcfb3a73cb2537af9e2c8
Alexander Ivahnenko <alexander_i@fast-report.com>
2017-04-18 23:40:38 +0300

    add comments

---
+++ comments.js
@@ -0,0 +1,52 @@
+module.exports = {
+  myComments: [
+    {
+      id: 1,
+      user_id: 1,
+      phone: '',
+      text: 'Dark night steel there shining',
+    },
+    {
+      id: 2,
+      user_id: 1,
+      phone: '',
+      text: "Can't stop keep on climbing",
+    },
+    {
+      id: 3,
+      user_id: 1,
+      phone: '',
+      text: 'Looking for what I knew',
+    },
+    {
+      id: 4,
+      user_id: 1,
+      phone: '',
+      text: 'I got a friend, he once told me',
+    },
+    {
+      id: 5,
+      user_id: 1,
+      phone: '',
+      text: '"You got a friendship, you ain\'t lonely"',
+    },
+    {
+      id: 6,
+      user_id: 2,
+      phone: '',
+      text: 'I hit the sack',
+    },
+    {
+      id: 7,
+      user_id: 2,
+      phone: '',
+      text: 'I\'ve been to long, I\'m glad to be back',
+    },
+    {
+      id: 8,
+      user_id: 2,
+      phone: '',
+      text: 'Yes, I\'m let loose, from a noose',
+    },
+  ]
+}
